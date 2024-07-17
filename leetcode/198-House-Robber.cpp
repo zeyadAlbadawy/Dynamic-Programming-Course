daf1e27e55aebd5a9240e836b4d69e4a6f2d7ffc
zeyad albadawy <99131483+zeyadAlbadawy@users.noreply.github.com>
2024-07-17 07:08:45 +0300

    [198] [Time Beats: 100.00%] [Memory Beats: 9.12%] - LeetPush

--- leetcode/198-House-Robber.cpp
+++ leetcode/198-House-Robber.cpp
@@ -3,13 +3,15 @@ const static auto _ = [] { std::ios::sync_with_stdio(false); std::cin.tie(nullpt
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
-        vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        dp[1] = max(nums[1], nums[0]);
-        
-        for(int i = 2; i < nums.size(); i++) 
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
-        return dp[nums.size() - 1];
+        if(nums.size() == 1)
+          return nums[0];
+        int robF = nums[0], robS = max(nums[0], nums[1]), temp = 0;
+
+        for(int i = 2; i < nums.size(); i++) {
+            temp = max(robF + nums[i], robS);
+            robF = robS;
+            robS = temp;
+        }
+        return robS;
     }
 };
