8c22dc464ca297114b27a7a782d8fe42a27481aa
zeyad albadawy <99131483+zeyadAlbadawy@users.noreply.github.com>
2024-08-19 00:09:06 +0300

    [91] [Time Beats: 100.00%] [Memory Beats: 74.38%] - LeetPush

--- leetcode/91-Decode-Ways.cpp
+++ leetcode/91-Decode-Ways.cpp
@@ -1,24 +1,34 @@
 class Solution {
-public:
-    int DFS(int i, string s, vector<int> &dp) {
-        // Base Cases
-        if(i == s.size()) 
-          return 1;
-        if(s[i] == '0' )
-          return 0;
-        if(dp[i] != -1)
-          return dp[i];
-        
-        int res = DFS(i + 1, s, dp);
-        if((i + 1 < s.size()) && (s[i] == '1' || (s[i] == '2' && s[i + 1] >= '0' && s[i + 1] <= '6')))
-          res += DFS(i + 2, s, dp);
-        dp[i] = res;
-        return res;
-    }
+    public: 
+    int getTheFullCount(string &s, int i, vector<int> &dp) {
 
+       \t// Base Case
+        if (i > s.size() || s[i] == '0')
+            return 0;
+
+       \t// Where I Should Terminate And Increment The final res
+        if (i == s.size())
+            return 1;
+
+       \t// For Memoization
+        if (dp[i] != -1)
+            return dp[i];
+
+       \t// You Have Two Choices
+       \t// 1- get only one num at a time
+       \t// 2- get Two numbers at a time
+
+        int finalRes = 0;
+        finalRes += getTheFullCount(s, i + 1, dp);
+
+        if (i + 1 < s.size() && ((s[i] == '2' && s[i + 1] <= '6') || (s[i] == '1')))
+            finalRes += getTheFullCount(s, i + 2, dp);
+
+        dp[i] = finalRes;
+        return finalRes;
+    }
     int numDecodings(string s) {
-        if(s.empty()) return 0;
         vector<int> dp(s.size(), -1);
-        return DFS(0, s, dp);
+        return  getTheFullCount(s, 0, dp);
     }
 };
