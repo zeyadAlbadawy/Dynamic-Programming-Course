84e2b98068455ef31ea3883803ac001fff903c77
zeyad albadawy <99131483+zeyadAlbadawy@users.noreply.github.com>
2025-02-07 23:34:09 +0200

    [1] [Time Beats: 74.40%] [Memory Beats: 21.50%] - LeetPush

---
+++ LeetCode Top 150/1-Two-Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.find(target - nums[i]) != mp.end()) 
+               return {i, mp[target - nums[i]]};
+            else mp[nums[i]] = i;
+        }
+        return {0,0};
+    }
+};
