465ea2db2b079b7a71ee98e264110203ca3aaaf9
zeyad albadawy <99131483+zeyadAlbadawy@users.noreply.github.com>
2025-02-12 20:51:51 +0200

    [71] [Time Beats: 5.24%] [Memory Beats: 56.14%] - LeetPush

---
+++ LeetCode Top 150/71-Simplify-Path.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> tokens;
+        istringstream iss(path);
+        string token;
+
+        while(getline(iss, token, '/')) {
+            if(token == \.\ || token == \\)
+               continue;
+            tokens.push_back(token);
+            cout << token << endl;
+        }
+
+        // Handle the .. which means one step back for each
+        vector<string> stack;
+
+        // If it is Relative path, mention that by push empty string 
+        if(path[0] == '/')
+            stack.push_back(\\);
+
+        for(auto token : tokens) {
+            if(token == \..\) {
+                // check if the stack is empty then it must be pushed OR the
+               // Prev element is .. then it can't be removed untill now
+                if(!stack.size() || stack[stack.size() - 1] == \..\)
+                   stack.push_back(token);
+                else if(stack[stack.size() - 1] != \\) 
+                   stack.pop_back();
+            } else 
+              stack.push_back(token);
+        }
+
+        // Combine all the elements in the stack and give the finalRes
+        if(stack.size() == 0 && stack[stack.size() - 1] == \\)
+           return \/\;
+        
+        path.clear();
+        for(int i = 0; i < stack.size(); i++) {
+            path += stack[i] + '/';
+        }
+        if(path.size() != 1)
+           path.pop_back();
+        // path = '/' + path;
+        return path;
+    }
+};
